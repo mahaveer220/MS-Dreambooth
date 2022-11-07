fbbc46ce8ccc43dc4ef8173e5e6c4af6fe06803e
mahaveer <37685-mahaveer@users.noreply.gitlab.aicrowd.com>
2022-11-07 00:16:38 -0500

    init code push

---
+++ train.sh
@@ -0,0 +1,25 @@
+export MODEL_NAME="CompVis/stable-diffusion-v1-4"
+export OUTPUT_DIR="output"
+
+accelerate launch train_dreambooth.py \
+  --pretrained_model_name_or_path=$MODEL_NAME \
+  --pretrained_vae_name_or_path="stabilityai/sd-vae-ft-mse" \
+  --output_dir=$OUTPUT_DIR \
+  --revision="fp16" \
+  --with_prior_preservation --prior_loss_weight=1.0 \
+  --seed=1337 \
+  --resolution=512 \
+  --train_batch_size=1 \
+  --train_text_encoder \
+  --mixed_precision="fp16" \
+  --use_8bit_adam \
+  --gradient_accumulation_steps=1 \
+  --learning_rate=1e-6 \
+  --lr_scheduler="constant" \
+  --lr_warmup_steps=0 \
+  --num_class_images=50 \
+  --sample_batch_size=4 \
+  --max_train_steps=1000 \
+  --save_interval=500 \
+  --save_sample_prompt="photo of a lmwq cat playing with yted ball" \
+  --concepts_list="concepts_list.json"
